c4987dd1daffe920cdc29d1207997d6cb76bb274
Rayhan-abdullha <captainrayhan215@gmail.com>
2026-01-15 22:17:04 +0600

    move zero solved using two pointer

--- array/main.go
+++ array/main.go
@@ -3,7 +3,23 @@ import "fmt"
 
 func Array(){
 	// binary search tree
+	/*
 	arr := []int{1,2,3,4,55,6,7}
 	res := BinarySearch(arr, 3)
 	fmt.Println(res)
+	*/
+
+	// zeroes move brute force
+	/*
+	arr2 := []int{1,2,0,4,0,6,7}
+	MoveZeroes(arr2)
+	fmt.Println(arr2)
+
+	arr3 := []int{1,0,12,0,4,3,6,7}
+	MoveZeroes1(arr3) //two pinter
+	fmt.Println(arr3)
+	*/
+	arr3 := []int{1,0,12,0,4,3,6,7}
+	MoveZeroes1(arr3) //two pinter
+	fmt.Println(arr3)
 }
---
+++ array/move_zero.go
@@ -0,0 +1,43 @@
+package array
+
+func MoveZeroes(nums []int) {
+	n := len(nums)
+	result := make([]int, n)
+	j := 0
+
+	// First pass: accumulate non-zero elements
+	for i := 0; i < n; i++ {
+		if nums[i] != 0 {
+			result[j] = nums[i]
+			j++
+		}
+	}
+	
+	// Second pass: fill remaining positions with zeroes
+	for ; j < n; j++ {
+		result[j] = 0
+	}
+
+	// Copy back to original slice
+	for i := 0; i < n; i++ {
+		nums[i] = result[i]
+	}
+}
+
+// two pointer
+func MoveZeroes1(nums []int) {
+	writePos := 0 
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != 0 {
+			nums[writePos] = nums[i]
+			writePos++
+		}
+	}
+
+
+	for writePos < len(nums) {
+		nums[writePos] = 0
+		writePos++
+	}
+}
